d4fe93e197b7ea76a6a64dd3acb1617272987afb
thomastaojin <73690787+thomastaojin@users.noreply.github.com>
2022-06-07 15:44:56 +0800

    完成了登入功能
    微信支付自己用了一个假的token，此功能没有做

--- components/my-settle/my-settle.vue
+++ components/my-settle/my-settle.vue
@@ -12,21 +12,29 @@
     </view>
 
     <!-- 结算按钮 -->
-    <view class="btn-settle">结算({{checkedCount}})</view>
+    <view class="btn-settle" @click="settlement">结算({{checkedCount}})</view>
   </view>
 </template>
 
 <script>
-  import { mapGetters, mapMutations } from 'vuex'
+  import { mapGetters, mapMutations, mapState } from 'vuex'
 
   export default {
     name: "my-settle",
     data() {
-      return {}
+      return {
+        // 倒计时的秒数
+        seconds: 3,
+        // 定时器的 Id
+        timer: null
+      }
     },
 
     computed: {
       ...mapGetters('m_cart', ['total', 'checkedCount', 'checkedGoodsAmount']),
+      ...mapGetters('m_user', ['addstr']),
+      ...mapState('m_user', ['token']) ,
+      
       isFullCheck() {
         return this.total === this.checkedCount
       },
@@ -34,11 +42,75 @@
 
     methods: {
       ...mapMutations('m_cart', ['updateAllGoodsState']),
+      ...mapMutations('m_user', ['updateRedirectInfo']),
       changeAllState() {
         // 修改购物车中所有商品的选中状态
         // !this.isFullCheck 表示：当前全选按钮的状态取反之后，就是最新的勾选状态
         this.updateAllGoodsState(!this.isFullCheck)
       },
+      
+      // 点击了结算按钮
+      settlement() {
+        // 1. 先判断是否勾选了要结算的商品
+        if(!this.checkedCount) return uni.$showMsg('请选择要结算的商品！')
+        
+        // 2. 再判断用户是否选择了收货地址
+        if (!this.addstr) return uni.$showMsg('请选择收货地址！')
+        
+        // 3. 最后判断用户是否登录了，如果没有登录，则调用 delayNavigate() 进行倒计时的导航跳转
+        if (!this.token) return this.delayNavigate()
+      },
+      
+      // 延迟导航到 my 页面
+      delayNavigate() {
+        // 1. 展示提示消息，此时 seconds 的值等于 3
+        this.seconds = 3
+        this.showTips(this.seconds)
+        
+        // 2. 创建定时器，每隔 1 秒执行一次
+        this.timer = setInterval(() => {
+          this.seconds--
+          // 判断秒数是否 <= 0
+          if (this.seconds <= 0) {
+            // 清除定时器
+            clearInterval(this.timer)
+            // 跳转到 my 页面
+            uni.switchTab({
+              url: '/pages/my/my',
+              // 页面跳转成功之后的回调函数
+              success: () => {
+                // 调用 vuex 的 updateRedirectInfo 方法，把跳转信息存储到 Store 中
+                this.updateRedirectInfo({
+                  // 跳转的方式
+                  openType: 'switchTab',
+                  // 从哪个页面跳转过去的
+                  from: '/pages/cart/cart'
+                })
+              }
+            })
+            
+            // 终止后续代码的运行（当秒数为 0 时，不再展示 toast 提示消息
+            return
+          }
+          
+          this.showTips(this.seconds)
+        }, 1000)
+      },
+      
+      // 展示倒计时的提示消息
+      showTips(n) {
+        // 调用 uni.showToast() 方法，展示提示消息
+        uni.showToast({
+          // 不展示任何图标
+          icon: 'none',
+          // 提示的消息
+          title: '请登录后再结算！' + n + ' 秒后自动跳转到登录页',
+          // 为页面添加透明遮罩，防止点击穿透
+          mask: true,
+          // 1.5 秒后自动消失
+          duration: 1500
+        })
+      }
     },
 
   }
